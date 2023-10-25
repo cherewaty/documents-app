287e9aadd208e600f874f7a677ac18dc125a166a
Jeffrey Cherewaty <jeffrey@cherewaty.com>
2023-10-25 15:06:35 -0700

    Set up Mock Service Worker

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "@fontsource/inter";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function deferRender() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser");
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+deferRender().then(() => {
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
