f93a2e5c1b52af060715783bc9489692d6ee23d1
Jeffrey Cherewaty <jeffrey@cherewaty.com>
2023-10-30 18:29:48 -0700

    Replace yarn with npm

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import App from "./App";
 import "@fontsource/inter";
 
 async function deferRender() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
+  // if (process.env.NODE_ENV !== "development") {
+  //   return;
+  // }
 
   const { worker } = await import("./mocks/browser");
 
